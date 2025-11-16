eb28f822de83d415e981fc55f19dbb2062c5b590
Vishal Prajapati <64828902+vishalvx@users.noreply.github.com>
2025-11-16 21:54:32 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name="medical-chatbot",
+    version="0.1.0",        
+    author="jinal prajapati",
+    author_email="jinalprajapati1183@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
